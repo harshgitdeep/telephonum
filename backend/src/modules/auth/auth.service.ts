9d37d4d1b6862d51603aef1229839af6ba22bb41
harshgitdeep <harshzone3@gmail.com>
2026-07-20 15:57:41 +0530

    feat: implement bcrypt password hashing

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import User from "./auth.model";
 
 export const registerUser = async (userData: any) => {
@@ -12,10 +13,12 @@ export const registerUser = async (userData: any) => {
     };
   }
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await User.create({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
 
   return {
@@ -37,7 +40,12 @@ export const loginUser = async (userData: any) => {
     };
   }
 
-  if (user.password !== password) {
+  const isPasswordCorrect = await bcrypt.compare(
+  password,
+  user.password
+  );
+
+  if (!isPasswordCorrect) {
     return {
       success: false,
       message: "Invalid password",
